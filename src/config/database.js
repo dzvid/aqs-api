743d6a7903fe278d3cd6ffd37150f03d21801fd6
tukno <oliveiradavid007@gmail.com>
2019-09-21 18:39:36 -0400

    chore: Enviroment variables configuration using Dotenv

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+/** Database configurations */
+require('dotenv/config');
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
