e0a3cc7fa4f3a3e6713a8d06c58ad69fd13d0846
tukno <oliveiradavid007@gmail.com>
2019-09-21 20:46:05 -0400

    chore: Create migration sensor_nodes

---
+++ src/database/migrations/20190922004814-create-sensor_nodes.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('sensor_nodes', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      id_sensor_node: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+
+      eid: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      // Timestamps
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('sensor_nodes');
+  },
+};
