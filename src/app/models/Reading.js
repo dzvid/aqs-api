2effacb393bfd03561d4333c3e90bfe60b84d554
tukno <oliveiradavid007@gmail.com>
2019-09-23 20:27:03 -0400

    chore: Created Reading model - Reading Model created; - Included in the Models Loader:   - The Reading model;   - Method to create relationship between models.

---
+++ src/app/models/Reading.js
@@ -0,0 +1,34 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Reading extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        // Data collected
+        humidity: Sequelize.DOUBLE,
+        temperature: Sequelize.DOUBLE,
+        pressure: Sequelize.DOUBLE,
+        ozone: Sequelize.DOUBLE,
+        pm25: Sequelize.DOUBLE,
+        carbon_monoxide: Sequelize.DOUBLE,
+        collected_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    // Returns the model created
+    return this;
+  }
+
+  // Defines a relationship between tables sensor_nodes and readings, add FK in readings
+  static associate(models) {
+    this.belongsTo(models.SensorNode, {
+      foreignKey: 'sensor_node_id',
+      as: 'sensor_node',
+    });
+  }
+}
+
+export default Reading;
--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,13 @@
 import Sequelize from 'sequelize';
 
+// Import models
 import SensorNode from '../app/models/SensorNode';
+import Reading from '../app/models/Reading';
 
 import databaseConfig from '../config/database';
 
 // List of Models to be initialized/loaded
-const models = [SensorNode];
+const models = [SensorNode, Reading];
 
 class Database {
   constructor() {
@@ -13,14 +15,17 @@ class Database {
   }
 
   /**
-   * Connects to the database, initialize and load the models
+   * Connects to the database, initialize/load the models and respective
+   * relationships between models (if there is any).
    */
   init() {
     // Database connection
     this.connection = new Sequelize(databaseConfig);
 
-    // Load the models
-    models.map(model => model.init(this.connection));
+    // Load the models and relationships (if there are some)
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
