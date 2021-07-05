496e41af9edfdf592b2c110ea5e72ead0faa5bc4
David <oliveiradavid.dev@gmail.com>
2021-07-04 23:34:44 -0400

    chore: Add swagger docs
    WIP

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,7 +4,9 @@ import 'express-async-errors';
 
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 
 /** Dependency injection */
 import '@shared/container';
@@ -13,14 +15,15 @@ import errorHandler from '@shared/infra/http/middlewares/errorHandler';
 import { router } from '@shared/infra/http/routes';
 import createConnection from '@shared/infra/typeorm';
 
-import swaggerConfig from '../../../swagger.json';
+const swaggerDocumentPath = path.join(__dirname, '../../../swagger.yaml');
+const swaggerDocument = yaml.load(swaggerDocumentPath);
 
 createConnection();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', router);
 app.use(errorHandler);
 
