74f0b524a45a1f1f55462d5bd3a77869a02ef1fa
David <oliveiradavid.dev@gmail.com>
2021-07-12 22:28:55 -0400

    chore: Build project with babel

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,13 @@
-module.exports = {
+const connectionSetup = {
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+};
+
+const devConfig = {
   entities: [
     './src/modules/**/infra/typeorm/entities/*.ts',
     './src/modules/**/entities/*.ts',
@@ -13,4 +16,20 @@ module.exports = {
   cli: {
     migrationsDir: './src/shared/infra/typeorm/migrations',
   },
+  ...connectionSetup,
 };
+
+const prodConfig = {
+  entities: [
+    './dist/modules/**/infra/typeorm/entities/*.js',
+    './dist/modules/**/entities/*.js',
+  ],
+  migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
+  cli: {
+    migrationsDir: './dist/shared/infra/typeorm/migrations',
+  },
+  ...connectionSetup,
+};
+
+module.exports =
+  process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
